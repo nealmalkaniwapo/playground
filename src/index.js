a601daf0d3665524003844731ccc016696537a1c
Neal Malkani <Neal.Malkani@washpost.com>
2018-11-27 11:59:05 -0500

    added react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log("hello");
+
+const Index = () => {
+  return <div>Hello World!</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));
